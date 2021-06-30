8adbc571a22f45205f3181674c8c21cd2251dc8f
sankaja <sankajagunasekara@gmail.com>
2021-06-30 19:28:27 +0530

    Completed app

--- lib/screen2.dart
+++ lib/screen2.dart
@@ -12,7 +12,10 @@ class Screen2 extends StatelessWidget {
         child: RaisedButton(
           color: Colors.blue,
           child: Text('Go Back To Screen 1'),
-          onPressed: () {},
+          onPressed: () {
+            Navigator.pushNamed(context, '/first');
+            //Navigator.pop(context);
+          },
         ),
       ),
     );
